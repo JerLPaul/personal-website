386ca7e54ff7c29e0938b95321df75e926c358b8
Jerrell Paul <jerrell@uoguelph.ca>
2023-12-18 16:47:35 -0500

    Added routers to the nav bar and created the new pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div class="App">
-      <body class="App-body">
-        <Navbar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </body>
-    </div>
+    <Router>
+      <div class="App">
+        <body class="App-body">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </body>
+      </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+import './App.css';
+
 export default function Navbar() {
     return (
         <nav>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
+                <li>
+                    <Link to="/" className="navbtn">Home</Link>
+                </li>
+                <li><Link to="/about" className="navbtn">About</Link></li>
+                <li><Link to="/projects" className="navbtn">Projects</Link></li>
             </ul>
         </nav>
     );
